67b59e31c58854676e912e4020f6d2755a1ee721
ItzhakLevy1 <itzhaklevydev@gmail.com>
2024-10-26 14:25:17 +0300

    add tsconfig.json to handle the compilerOptions

---
+++ 01-helloworld/02-variables/03-loops-and-arrays/04-classes/03-tsconfig/Customer.ts
@@ -0,0 +1,33 @@
+class Customer {
+    private _firstName: string;
+    private _lastName: string;
+  
+    constructor(theFirst: string, theLast: string) {
+      this._firstName = theFirst;
+      this._lastName = theLast;
+    }
+  
+    public get firstName(): string {
+      return this._firstName;
+    }
+  
+    public set firstName(value: string) {
+      this._firstName = value;
+    }
+  
+    public get lastName(): string {
+      return this._lastName;
+    }
+  
+    public set lastName(value: string) {
+      this._lastName = value;
+    }
+  }
+  
+  // Create an instance of the Customer class
+  let myCustomer = new Customer("Susan", "Didi");
+  console.log("Original first name: " + myCustomer.firstName); // Get the first name using the getter
+  
+  myCustomer.firstName = "Dana"; // Change the first name useing the setter
+  console.log("Modified first name: " + myCustomer.firstName); // Get the modified first name using the getter
+  
