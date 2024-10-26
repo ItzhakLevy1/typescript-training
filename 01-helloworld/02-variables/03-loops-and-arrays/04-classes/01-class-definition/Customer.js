9c917631739e7b5e7b70a8fdc0baf97c0db2a970
ItzhakLevy1 <itzhaklevydev@gmail.com>
2024-10-26 13:21:17 +0300

    TypeScript set and get instead of setters and getters

--- 01-helloworld/02-variables/03-loops-and-arrays/04-classes/01-class-definition/Customer.js
+++ 01-helloworld/02-variables/03-loops-and-arrays/04-classes/01-class-definition/Customer.js
@@ -1,18 +1,22 @@
 var Customer = /** @class */ (function () {
     function Customer(theFirst, theLast) {
-        this.firstName = theFirst;
-        this.lastName = theLast;
+        this._firstName = theFirst;
+        this._lastName = theLast;
     }
-    Customer.prototype.getFirstName = function () {
-        return this.firstName;
-    };
-    Customer.prototype.setFirstName = function (theFirst) {
-        this.firstName = theFirst;
-    };
+    Object.defineProperty(Customer.prototype, "firstName", {
+        get: function () {
+            return this._firstName;
+        },
+        set: function (value) {
+            this._firstName = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
     return Customer;
 }());
 // Create an instance of the Customer class
-var myCustomer = new Customer("Martin", "Dixon");
-console.log("Original first name: " + myCustomer.getFirstName()); // Get the first name using the getter
-myCustomer.setFirstName("Greg"); // Change the first name useing the setter
-console.log("Modified first name: " + myCustomer.getFirstName()); // Get the modified first name using the getter
+var myCustomer = new Customer("Susan", "Didi");
+console.log("Original first name: " + myCustomer.firstName); // Get the first name using the getter
+myCustomer.firstName = "Dana"; // Change the first name useing the setter
+console.log("Modified first name: " + myCustomer.firstName); // Get the modified first name using the getter
--- 01-helloworld/02-variables/03-loops-and-arrays/04-classes/01-class-definition/Customer.ts
+++ 01-helloworld/02-variables/03-loops-and-arrays/04-classes/01-class-definition/Customer.ts
@@ -1,24 +1,24 @@
 class Customer {
-  private firstName: string;
-  private lastName: string;
+  private _firstName: string;
+  private _lastName: string;
 
   constructor(theFirst: string, theLast: string) {
-    this.firstName = theFirst;
-    this.lastName = theLast;
+    this._firstName = theFirst;
+    this._lastName = theLast;
   }
 
-  public getFirstName(): string {
-    return this.firstName;
+  public get firstName(): string {
+    return this._firstName;
   }
 
-  public setFirstName(theFirst: string): void {
-    this.firstName = theFirst;
+  public set firstName(value: string) {
+    this._firstName = value;
   }
 }
 
 // Create an instance of the Customer class
-let myCustomer = new Customer("Martin", "Dixon");
-console.log("Original first name: " + myCustomer.getFirstName()); // Get the first name using the getter
+let myCustomer = new Customer("Susan", "Didi");
+console.log("Original first name: " + myCustomer.firstName); // Get the first name using the getter
 
-myCustomer.setFirstName("Greg"); // Change the first name useing the setter
-console.log("Modified first name: " + myCustomer.getFirstName()); // Get the modified first name using the getter
+myCustomer.firstName = "Dana"; // Change the first name useing the setter
+console.log("Modified first name: " + myCustomer.firstName); // Get the modified first name using the getter
