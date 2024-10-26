56797529de97b208033e2d09f6c4e85cfb3dc37d
ItzhakLevy1 <itzhaklevydev@gmail.com>
2024-10-26 12:59:08 +0300

    TypeScript getters and setters

--- 01-helloworld/02-variables/03-loops-and-arrays/04-classes/01-class-definition/Customer.js
+++ 01-helloworld/02-variables/03-loops-and-arrays/04-classes/01-class-definition/Customer.js
@@ -3,9 +3,16 @@ var Customer = /** @class */ (function () {
         this.firstName = theFirst;
         this.lastName = theLast;
     }
+    Customer.prototype.getFirstName = function () {
+        return this.firstName;
+    };
+    Customer.prototype.setFirstName = function (theFirst) {
+        this.firstName = theFirst;
+    };
     return Customer;
 }());
 // Create an instance of the Customer class
 var myCustomer = new Customer("Martin", "Dixon");
-console.log(myCustomer.firstName);
-console.log(myCustomer.lastName);
+console.log("Original first name: " + myCustomer.getFirstName()); // Get the first name using the getter
+myCustomer.setFirstName("Greg"); // Change the first name useing the setter
+console.log("Modified first name: " + myCustomer.getFirstName()); // Get the modified first name using the getter
--- 01-helloworld/02-variables/03-loops-and-arrays/04-classes/01-class-definition/Customer.ts
+++ 01-helloworld/02-variables/03-loops-and-arrays/04-classes/01-class-definition/Customer.ts
@@ -1,16 +1,24 @@
 class Customer {
+  private firstName: string;
+  private lastName: string;
 
-    firstName: string;
-    lastName: string;
+  constructor(theFirst: string, theLast: string) {
+    this.firstName = theFirst;
+    this.lastName = theLast;
+  }
 
-    constructor (theFirst: string, theLast: string){
-        this.firstName = theFirst;
-        this.lastName = theLast;
-    }
+  public getFirstName(): string {
+    return this.firstName;
+  }
+
+  public setFirstName(theFirst: string): void {
+    this.firstName = theFirst;
+  }
 }
 
 // Create an instance of the Customer class
 let myCustomer = new Customer("Martin", "Dixon");
+console.log("Original first name: " + myCustomer.getFirstName()); // Get the first name using the getter
 
-console.log(myCustomer.firstName);
-console.log(myCustomer.lastName);
+myCustomer.setFirstName("Greg"); // Change the first name useing the setter
+console.log("Modified first name: " + myCustomer.getFirstName()); // Get the modified first name using the getter
