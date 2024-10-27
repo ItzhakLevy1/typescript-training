379baece56c256cb2271fc48cdefdba4f9138852
ItzhakLevy1 <itzhaklevydev@gmail.com>
2024-10-27 09:04:18 +0200

    Create an instance of the Customer class after importing it

---
+++ 01-helloworld/02-variables/03-loops-and-arrays/04-classes/05-modules/Driver.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Customer_1 = require("./Customer");
+// Creating an instance of the class
+let myCustomer = new Customer_1.Customer("Martin2", "Dixon2");
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
---
+++ 01-helloworld/02-variables/03-loops-and-arrays/04-classes/05-modules/Driver.ts
@@ -0,0 +1,7 @@
+import { Customer } from "./Customer";
+
+// Creating an instance of the Customer class
+let myCustomer = new Customer("Martin2", "Dixon2");
+
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
